81e0864a8b3c21793b15b13b5713c4ea962b612c
Amr Ali <amraliahmed25@gmail.com>
2023-11-03 20:31:19 +0200

    Chapter 6 self exercise

--- src/chapter6/PhoneBillOop.java
+++ src/chapter6/PhoneBillOop.java
@@ -3,7 +3,7 @@ package chapter6;
 public class PhoneBillOop {
 
     public static void main(String args[]){
-        // call the method PhoneBill and pass to it the id , minutes used , allotted minutes, base cost //
+        //** call the method PhoneBill and pass to it the id , minutes used , allotted minutes, base cost **//
         PhoneBill phoneBill = new PhoneBill(1,800,800,50.9);
         phoneBill.printItemizedBill();
 
